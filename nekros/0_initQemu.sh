a80d2a4df711226821889aab2c6b9209f47980ae
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2017-03-03 20:28:43 +0100

    Add Ubuntu 1404

--- nekros/0_initQemu.sh
+++ nekros/0_initQemu.sh
@@ -3,11 +3,14 @@
 mkdir -p ~/qemu/ubuntu1604_64/bin
 mkdir -p ~/qemu/ubuntu1504_32/bin
 mkdir -p ~/qemu/ubuntu1404_64/bin
+mkdir -p ~/qemu/ubuntu1404_32/bin
 
 cp ubuntu1604_64/bin/* ~/qemu/ubuntu1604_64/bin
 cp ubuntu1504_32/bin/* ~/qemu/ubuntu1504_32/bin
 cp ubuntu1404_64/bin/* ~/qemu/ubuntu1404_64/bin
+cp ubuntu1404_32/bin/* ~/qemu/ubuntu1404_32/bin
 
 chmod 755 ~/qemu/ubuntu1604_64/bin/*
 chmod 755 ~/qemu/ubuntu1504_32/bin/*
 chmod 755 ~/qemu/ubuntu1404_64/bin/*
+chmod 755 ~/qemu/ubuntu1404_32/bin/*
--- nekros/test.sh
+++ nekros/test.sh
@@ -6,6 +6,15 @@ currentuser="$(whoami)"
 
 echo "Run Qemu"
 
+echo "ubuntu1404_32"
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/media/$currentuser/nekros/ubuntu1404_32/lib/ ~/qemu/ubuntu1404_32/bin/qemu-system-i386  -k fr -sdl -localtime \
+                    -drive file=/media/$currentuser/nekros/vhda/vhda.qcow2,format=qcow2,index=0,media=disk \
+                    -drive file=/media/$currentuser/nekros/vhdb/vhdb.qcow2,format=qcow2,index=1,media=disk \
+                    -L /media/$currentuser/nekros/qemu/pc-bios \
+                    -enable-kvm -k fr -m 1024 -smp 2 
+
+
 echo "ubuntu1404_64"
 
 #LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/media/$currentuser/nekros/ubuntu1404_64/lib/ ~/qemu/ubuntu1404_64/bin/qemu-system-x86_64  -k fr -sdl -localtime \
@@ -17,8 +26,8 @@ echo "ubuntu1404_64"
 
 echo "ubuntu1604_64"
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/media/$currentuser/nekros/ubuntu1604_64/lib/ ~/qemu/ubuntu1604_64/bin/qemu-system-x86_64  -k fr -sdl -localtime \
-                    -drive file=/media/$currentuser/nekros/vhda/vhda.qcow2,format=qcow2,index=0,media=disk \
-                    -drive file=/media/$currentuser/nekros/vhdb/vhdb.qcow2,format=qcow2,index=1,media=disk \
-                    -L /media/$currentuser/nekros/qemu/pc-bios \
-                    -enable-kvm -k fr -m 1024 -smp 2 
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/media/$currentuser/nekros/ubuntu1604_64/lib/ ~/qemu/ubuntu1604_64/bin/qemu-system-x86_64  -k fr -sdl -localtime \
+#                    -drive file=/media/$currentuser/nekros/vhda/vhda.qcow2,format=qcow2,index=0,media=disk \
+#                    -drive file=/media/$currentuser/nekros/vhdb/vhdb.qcow2,format=qcow2,index=1,media=disk \
+#                    -L /media/$currentuser/nekros/qemu/pc-bios \
+#                    -enable-kvm -k fr -m 1024 -smp 2 
